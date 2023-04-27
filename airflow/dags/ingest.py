93e7ce45a6fe53e20f73ebe357e8f9aa106c2aaf
green-nk <green.nk21@gmail.com>
2023-04-27 10:54:18 +0000

    add upload staging data to gcs task

--- airflow/dags/ingest.py
+++ airflow/dags/ingest.py
@@ -20,6 +20,7 @@ def ingest_nyc_citi_bike():
     DATA_FILE = "201907-citibike-tripdata"
     BUCKET_NAME = Variable.get("gcs_bucket")
 
+    # Extract data from a source
     src = f"https://s3.amazonaws.com/tripdata/{DATA_FILE}.csv.zip"
     out = f"/opt/airflow/data/raw/{DATA_FILE}.csv.zip"
     extract_nyc_citi_bike = BashOperator(
@@ -27,6 +28,7 @@ def ingest_nyc_citi_bike():
         bash_command=f"/opt/airflow/dags/scripts/bash/extract.sh {src} {out}"
     )
 
+    # Load raw data into a data lake
     raw_dst_blob = f"raw/{DATA_FILE}.csv.zip"
     nyc_citi_bike_to_raw_data_lake = LocalFilesystemToGCSOperator(
         task_id="nyc_citi_bike_to_raw_data_lake", 
@@ -35,6 +37,7 @@ def ingest_nyc_citi_bike():
         bucket=BUCKET_NAME
     )
 
+    # Preprocess raw data into staging data
     @task()
     def apply_raw_transformation(src, out, schema_filepath):
         """
@@ -53,7 +56,16 @@ def ingest_nyc_citi_bike():
     out = f"/opt/airflow/data/staging/{DATA_FILE}.parquet.gz"
     schema_filepath = "/opt/airflow/data/config/schema.json"
     transform_raw_nyc_citi_bike = apply_raw_transformation(src, out, schema_filepath)
+
+    # Load preprocessed data back into staging area in a data lake
+    staging_dst_blob = f"staging/{DATA_FILE}.parquet.gz"
+    nyc_citi_bike_to_staging_data_lake = LocalFilesystemToGCSOperator(
+        task_id="nyc_citi_bike_to_staging_data_lake", 
+        src=out, 
+        dst=staging_dst_blob, 
+        bucket=BUCKET_NAME
+    )
     
-    extract_nyc_citi_bike >> nyc_citi_bike_to_raw_data_lake >> transform_raw_nyc_citi_bike
+    extract_nyc_citi_bike >> nyc_citi_bike_to_raw_data_lake >> transform_raw_nyc_citi_bike >> nyc_citi_bike_to_staging_data_lake
 
 nyc_citi_bike = ingest_nyc_citi_bike()
